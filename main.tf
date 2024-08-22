4eb5d92f6337276070e4e0edd580483c5793f3e4
Yurii Kosyi <y.kosiy@seosmart.ua>
2024-08-22 18:04:59 +0300

    fix main

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "gke_cluster" {
   source         = "github.com/den-vasyliev/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  GKE_NUM_NODES  = 3
 }
 
 provider "flux" {
